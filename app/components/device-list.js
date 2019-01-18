86012d1930138999832f4f28d464f29b31d655eb
Márton Salomváry <salomvary@gmail.com>
2019-01-18 17:57:20 +0100

    Add placeholder when no device is connected

--- app/components/device-list.js
+++ app/components/device-list.js
@@ -1,13 +1,16 @@
 const {createElement: e} = require('react')
 
+const noDevice = e('option', null, 'No Device')
+
 module.exports = function DeviceList ({devices, selected, onChange}) {
-  const options = devices.map(({name, path}) =>
+  const options = devices.length > 0 ? devices.map(({name, path}) =>
     e('option', {key: path, value: path}, name)
-  )
+  ) : noDevice
 
   return (
     e('select', {
       value: selected || '',
+      disabled: devices.length < 1,
       className: 'devices',
       onChange: (event) => onChange(event.target.value)
     }, options)
